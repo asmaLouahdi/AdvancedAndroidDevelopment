1c79ce90c9a0175c4c965567b4b623eac64112f8
AndroWiiid <androwiiid@gmail.com>
2012-11-27 21:47:13 +0100

    Add
    Add new main activity with access to samples, tutoriel and source codes
    of the application.

--- src/com/siteduzero/android/MainActivity.java
+++ src/com/siteduzero/android/MainActivity.java
@@ -1,13 +1,34 @@
 package com.siteduzero.android;
 
-import android.app.ListActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends ListActivity {
+import com.siteduzero.android.samples.SamplesListActivity;
+
+public class MainActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		// TODO temp. Wait new main activity
+		setContentView(R.layout.activity_main);
+	}
+
+	public void goToSamples(View v) {
+		startActivity(new Intent(this, SamplesListActivity.class));
+	}
+
+	public void goToSdZ(View v) {
+		Intent i = new Intent(
+				Intent.ACTION_VIEW,
+				Uri.parse("http://www.siteduzero.com/tutoriel-3-704884-1-aller-plus-loin-dans-le-developpement-android.html"));
+		startActivity(i);
+	}
+
+	public void goToSourceCodes(View v) {
+		Intent i = new Intent(Intent.ACTION_VIEW,
+				Uri.parse("https://github.com/AndroWiiid/SdZAndroid"));
+		startActivity(i);
 	}
 }
