c32aebfe4c3baca2784ae14baa98a01ed1b04c7d
AndroWiiid <androwiiid@gmail.com>
2012-11-27 14:11:23 +0100

    Add feature
    Add feature in ActionBar chapter about split menu and
    ShareActionProvider.

--- src/com/siteduzero/android/actionbar/ActionBarActivity.java
+++ src/com/siteduzero/android/actionbar/ActionBarActivity.java
@@ -1,20 +1,24 @@
 package com.siteduzero.android.actionbar;
 
-import com.siteduzero.android.R;
-
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
+import com.siteduzero.android.R;
+
 public class ActionBarActivity extends Activity {
+	private ShareActionProvider mShareActionProvider = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
 	}
@@ -23,14 +27,30 @@ public class ActionBarActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_actionbar, menu);
-		return true;
+
+	    MenuItem item = menu.findItem(R.id.menu_share);
+	    mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+
+	    Intent intent = new Intent(Intent.ACTION_SEND);
+	    intent.setType("text/plain");
+	    intent.putExtra(Intent.EXTRA_TEXT, "Message");
+	    setShareIntent(intent);
+	    
+	    return true;
+	}
+	
+	private void setShareIntent(Intent shareIntent) {
+	    if (mShareActionProvider != null) {
+	        mShareActionProvider.setShareIntent(shareIntent);
+	    }
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			Toast.makeText(this, R.string.toast_add, Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, R.string.toast_home, Toast.LENGTH_SHORT)
+					.show();
 			return true;
 		case R.id.menu_add:
 			Toast.makeText(this, R.string.toast_add, Toast.LENGTH_SHORT).show();
