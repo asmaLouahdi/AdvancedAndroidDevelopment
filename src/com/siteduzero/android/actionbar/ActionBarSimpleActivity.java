4b9a808aa7172011a94238bd4746a0296d077096
AndroWiiid <androwiiid@gmail.com>
2013-04-16 00:25:29 +0200

    Add
    Add drop down menu in ActionBar chapter.

--- src/com/siteduzero/android/actionbar/ActionBarSimpleActivity.java
+++ src/com/siteduzero/android/actionbar/ActionBarSimpleActivity.java
@@ -1,6 +1,7 @@
 package com.siteduzero.android.actionbar;
 
 import android.app.ActionBar;
+import android.app.ActionBar.OnNavigationListener;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,7 +10,9 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.SearchView;
 import android.widget.SearchView.OnQueryTextListener;
+import android.widget.ArrayAdapter;
 import android.widget.ShareActionProvider;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
 import com.siteduzero.android.R;
@@ -21,31 +24,48 @@ public class ActionBarSimpleActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-	
-		ActionBar actionBar = getActionBar();
+
+		final ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+
+		final SpinnerAdapter spinnerAdapter = ArrayAdapter.createFromResource(
+				this, R.array.list_examples,
+				android.R.layout.simple_spinner_dropdown_item);
+		final OnNavigationListener onNavigationListener = new OnNavigationListener() {
+			@Override
+			public boolean onNavigationItemSelected(int itemPosition,
+					long itemId) {
+				// Implement this method to attach a behavior to your drop down
+				// menu.
+				return false;
+			}
+		};
+		actionBar.setListNavigationCallbacks(spinnerAdapter,
+				onNavigationListener);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_actionbar, menu);
-	
+
 		// ShareActionProvider
 		MenuItem itemProvider = menu.findItem(R.id.menu_share);
 		mShareActionProvider = (ShareActionProvider) itemProvider
 				.getActionProvider();
-	
+
 		Intent intent = new Intent(Intent.ACTION_SEND);
 		intent.setType("text/plain");
 		intent.putExtra(Intent.EXTRA_TEXT, "Message");
 		setShareIntent(intent);
-	
+
 		// SearchView
 		MenuItem itemSearch = menu.findItem(R.id.menu_search);
 		mSearchView = (SearchView) itemSearch.getActionView();
 		mSearchView.setOnQueryTextListener(new OnQueryTextListener() {
-	
+
 			@Override
 			public boolean onQueryTextSubmit(String query) {
 				Toast.makeText(getApplicationContext(),
@@ -53,13 +73,13 @@ public class ActionBarSimpleActivity extends Activity {
 						.show();
 				return true;
 			}
-	
+
 			@Override
 			public boolean onQueryTextChange(String newText) {
 				return false;
 			}
 		});
-	
+
 		return true;
 	}
 
