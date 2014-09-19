a2e69e17e55d7040ebb9ae70cd78e89db1e8d36d
GerardPaligot <androwiiid@gmail.com>
2014-09-20 00:50:30 +0200

    Update contextual action bar samples

--- App/src/main/java/com/siteduzero/android/actionbar/ActionBarContextualActivity.java
+++ App/src/main/java/com/siteduzero/android/actionbar/ActionBarContextualActivity.java
@@ -1,8 +1,8 @@
 package com.siteduzero.android.actionbar;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.ActionMode;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 import com.siteduzero.android.R;
 
-public class ActionBarContextualActivity extends Activity {
+public class ActionBarContextualActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,34 +20,35 @@ public class ActionBarContextualActivity extends Activity {
 	}
 
 	public void changeContextual(View view) {
-		startActionMode(new ActionMode.Callback() {
+        startSupportActionMode(new ActionMode.Callback() {
 
-			@Override
-			public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-				switch (item.getItemId()) {
-				case R.id.menu_trash:
-					Toast.makeText(getApplicationContext(),
-							R.string.toast_trash, Toast.LENGTH_SHORT).show();
-					return true;
-				}
-				return false;
-			}
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                // Handle click on contextual item menu.
+                switch (item.getItemId()) {
+                    case R.id.menu_trash:
+                        Toast.makeText(getApplicationContext(), R.string.toast_trash, Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
 
-			@Override
-			public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-				MenuInflater inflater = mode.getMenuInflater();
-				inflater.inflate(R.menu.menu_contextual, menu);
-				return true;
-			}
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                // Inflate the contextual menu.
+                MenuInflater inflater = mode.getMenuInflater();
+                inflater.inflate(R.menu.menu_contextual, menu);
+                return true;
+            }
 
-			@Override
-			public void onDestroyActionMode(ActionMode mode) {
-			}
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+            }
 
-			@Override
-			public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-				return false;
-			}
-		});
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                return false;
+            }
+        });
 	}
 }
--- App/src/main/java/com/siteduzero/android/actionbar/ActionBarContextualListActivity.java
+++ App/src/main/java/com/siteduzero/android/actionbar/ActionBarContextualListActivity.java
@@ -1,9 +1,8 @@
 package com.siteduzero.android.actionbar;
 
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.ActionMode;
-import android.view.ActionMode.Callback;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.view.ActionMode;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,59 +10,64 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.siteduzero.android.R;
 
-public class ActionBarContextualListActivity extends ListActivity {
+public class ActionBarContextualListActivity extends ActionBarActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contextual_list);
 
-		final ArrayAdapter<CharSequence> adapter = ArrayAdapter
-				.createFromResource(this, R.array.list_examples,
-						android.R.layout.simple_list_item_1);
-		setListAdapter(adapter);
+        final ListView list = (ListView) findViewById(R.id.action_bar_contextual_list);
 
-		getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
-			@Override
-			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
-				startActionMode(mActionMode);
-				return true;
-			}
-		});
-	}
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter
+                .createFromResource(this, R.array.list_examples,
+                        android.R.layout.simple_list_item_1);
+        list.setAdapter(adapter);
 
-	final Callback mActionMode = new ActionMode.Callback() {
+        list.setOnItemLongClickListener(new OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                           int arg2, long arg3) {
+                startSupportActionMode(mActionMode);
+                return true;
+            }
+        });
+    }
 
-		@Override
-		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-			switch (item.getItemId()) {
-			case R.id.menu_trash:
-				Toast.makeText(getApplicationContext(), R.string.toast_trash,
-						Toast.LENGTH_SHORT).show();
-				return true;
-			}
-			return false;
-		}
+    final ActionMode.Callback mActionMode = new ActionMode.Callback() {
 
-		@Override
-		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-			MenuInflater inflater = mode.getMenuInflater();
-			inflater.inflate(R.menu.menu_contextual, menu);
-			return true;
-		}
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            // Handle click on contextual item menu.
+            switch (item.getItemId()) {
+                case R.id.menu_trash:
+                    Toast.makeText(getApplicationContext(), R.string.toast_trash, Toast.LENGTH_SHORT).show();
+                    return true;
+            }
+            return false;
+        }
 
-		@Override
-		public void onDestroyActionMode(ActionMode mode) {
-		}
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            // Inflate the contextual menu.
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.menu_contextual, menu);
+            return true;
+        }
 
-		@Override
-		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
-			return false;
-		}
-	};
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            return false;
+        }
+    };
 
 }
